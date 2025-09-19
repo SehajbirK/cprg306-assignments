af7fd0daa23fb1bcc4dbfb85a585ffa82f70c891
Sehajbir Kaur <mistym0de@Mac.lan>
2025-09-19 02:36:15 -0600

    Add StudentInfo component for Week 2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,10 @@ export default function StudentInfo() {
   return (
     <div>
       <p>Sehajbir Kaur</p>
-      <Link href="https://github.com/SehajbirK/cprg306-assignments" target="_blank">
+      <Link
+        href="https://github.com/SehajbirK/cprg306-assignments"
+        target="_blank"
+      >
         My GitHub Repository
       </Link>
     </div>
