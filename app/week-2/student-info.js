9fede87ba104da5036edcf430aab2be6642fd72e
Sehajbir Kaur <mistym0de@Mac.lan>
2025-09-19 01:28:20 -0600

    Add Week 1 and Week 2 assignments

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Sehajbir Kaur</p>
+      <Link href="https://github.com/SehajbirK/cprg306-assignments" target="_blank">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
