ab78c73d6e38718a304375af4ab94b8e19add28f
Sehajbir Kaur <mistym0de@Mac.lan>
2025-09-26 21:02:23 -0600

    Force push Week-3 assignment fixed

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="border p-2 rounded mb-2">
-      <strong>{name}</strong> — {quantity} ({category})
+    <li className="border p-2 mb-2 rounded bg-gray-100">
+      <strong>{name}</strong> — Quantity: {quantity}, Category: {category}
     </li>
   );
 }
